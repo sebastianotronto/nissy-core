de6fa5b9122758f261675aa31fe3cb6e7bab92e5
Sebastiano Tronto <sebastiano@tronto.net>
2025-07-17 09:00:38 +0200

    Added unit tests for coord cp and epud

---
+++ test/080_invcoord_cp/invcoord_cp_tests.c
@@ -0,0 +1,32 @@
+#include "../test.h"
+
+#define FACT_8 40320
+
+int64_t coord_cp(cube_t);
+cube_t invcoord_cp(int64_t);
+
+void run(void) {
+	oriented_cube_t cube;
+	int64_t coord, coord2;
+
+	cube.orientation = 0;
+
+	/* Test all possible values for CP coordinate */
+	for (coord = 0; coord < FACT_8; coord++) {
+		cube.cube = invcoord_cp(coord);
+
+		if (!isconsistent(cube)) {
+			printf("Not consistent\n");
+			return;
+		}
+
+		coord2 = coord_cp(cube.cube);
+		if (coord != coord2) {
+			printf("Error: invcoord of %" PRId64
+			    " returns %" PRId64 "\n", coord, coord2);
+			return;
+		}
+	}
+
+	printf("All good\n");
+}
