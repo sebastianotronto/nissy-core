a676d598ff6286bdb64b2803dee515c1436aa3bb
Sebastiano Tronto <sebastiano@tronto.net>
2026-03-01 17:25:05 +0100

    Better macros

--- src/utils/wrapthread.h
+++ src/utils/wrapthread.h
@@ -1,61 +1,62 @@
 #if THREADS == 1
-#define WRAPTHREADS_NOTHREADS 1
-#endif
-
-#if !WRAPTHREADS_NOTHREADS
-
-	#if defined(__unix__)
-
-		#include <threads.h>
-
-		#define wrapthread_atomic _Atomic
-
-		#define wrapthread_return_t int
-		#define wrapthread_return_val 0
-
-		#define wrapthread_define_var_thread_t(x) thrd_t x
-		#define wrapthread_define_var_mutex_t(x) mtx_t x
-		#define wrapthread_define_struct_thread_t(x) thrd_t x
-		#define wrapthread_define_struct_mutex_t(x) mtx_t x
-
-		#define wrapthread_define_if_threads(T, x) T x
-
-		#define wrapthread_create(a, f, arg) thrd_create(a, f, arg)
-		#define wrapthread_join(a) thrd_join(a, NULL)
-		#define wrapthread_mutex_init(a) mtx_init(a, mtx_plain)
-		#define wrapthread_mutex_lock(a) mtx_lock(a)
-		#define wrapthread_mutex_unlock(a) mtx_unlock(a)
-
-	#elif !defined(__STDC_NO_THREADS__)
-
-		#include <pthread.h>
-
-		#define wrapthread_atomic _Atomic
-
-		#define wrapthread_return_t void *
-		#define wrapthread_return_val NULL
-
-		#define wrapthread_define_var_thread_t(x) pthread_t x
-		#define wrapthread_define_var_mutex_t(x) pthread_mutex_t x
-		#define wrapthread_define_struct_thread_t(x) pthread_t x
-		#define wrapthread_define_struct_mutex_t(x) pthread_mutex_t x
-
-		#define wrapthread_define_if_threads(T, x) T x
-
-		#define wrapthread_create(a, f, arg) \
-		    pthread_create(a, NULL, f, arg)
-		#define wrapthread_join(a) pthread_join(a, NULL)
-		#define wrapthread_mutex_init(a) pthread_mutex_init(a, NULL)
-		#define wrapthread_mutex_lock(a) pthread_mutex_lock(a)
-		#define wrapthread_mutex_unlock(a) pthread_mutex_unlock(a)
-
-	#else
-		#define WRAPTHREADS_NOTHREADS 1
+	#define WRAPTHREADS_NOTHREADS 1
+#elif defined(__unix__)
+	#define WRAPTHREADS_PTHREADS 1
+#elif !defined(__STDC_NO_THREADS__)
+	#define WRAPTHREADS_C11THREADS 1
+#elif defined(__has_include)
+	#if __has_include(<pthreads.h>)
+		#define WRAPTHREADS_PTHREADS 1
+	#elif __has_include(<threads.h>)
+		#define WRAPTHREADS_C11THREADS 1
 	#endif
-
 #endif
 
-#if WRAPTHREADS_NOTHREADS
+#if WRAPTHREADS_PTHREADS
+
+	#include <pthread.h>
+
+	#define wrapthread_atomic _Atomic
+
+	#define wrapthread_return_t void *
+	#define wrapthread_return_val NULL
+
+	#define wrapthread_define_var_thread_t(x) pthread_t x
+	#define wrapthread_define_var_mutex_t(x) pthread_mutex_t x
+	#define wrapthread_define_struct_thread_t(x) pthread_t x
+	#define wrapthread_define_struct_mutex_t(x) pthread_mutex_t x
+
+	#define wrapthread_define_if_threads(T, x) T x
+
+	#define wrapthread_create(a, f, arg) pthread_create(a, NULL, f, arg)
+	#define wrapthread_join(a) pthread_join(a, NULL)
+	#define wrapthread_mutex_init(a) pthread_mutex_init(a, NULL)
+	#define wrapthread_mutex_lock(a) pthread_mutex_lock(a)
+	#define wrapthread_mutex_unlock(a) pthread_mutex_unlock(a)
+
+#elif WRAPTHREADS_C11THREADS
+
+	#include <threads.h>
+
+	#define wrapthread_atomic _Atomic
+
+	#define wrapthread_return_t int
+	#define wrapthread_return_val 0
+
+	#define wrapthread_define_var_thread_t(x) thrd_t x
+	#define wrapthread_define_var_mutex_t(x) mtx_t x
+	#define wrapthread_define_struct_thread_t(x) thrd_t x
+	#define wrapthread_define_struct_mutex_t(x) mtx_t x
+
+	#define wrapthread_define_if_threads(T, x) T x
+
+	#define wrapthread_create(a, f, arg) thrd_create(a, f, arg)
+	#define wrapthread_join(a) thrd_join(a, NULL)
+	#define wrapthread_mutex_init(a) mtx_init(a, mtx_plain)
+	#define wrapthread_mutex_lock(a) mtx_lock(a)
+	#define wrapthread_mutex_unlock(a) mtx_unlock(a)
+
+#else
 
 	#define wrapthread_atomic
 
